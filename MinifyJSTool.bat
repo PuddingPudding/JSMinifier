073207b106d41b0a61a5e02c3fda1aab6a7bbe85
PuddingPudding <hwsh70119@gmail.com>
2018-05-04 18:17:04 +0800

    雖然批次檔中已經有原生的遞回方法(我們會用到遞回方法去找到資料夾底下的所有子資料夾中的js檔)，但是我們要避免去壓縮libs資料夾裡的東西 (這裡面的js檔都是Laya原生的，而且很大，要是跑去壓縮他們的話要壓很久) 因此，我目前先自己寫了找資料夾的遞回方法，但是目前又遇到了另一個麻煩，我沒辦法用一般的變數把最新版的jar檔存下來，也因此我沒辦法在遞回找資料夾的function中請closure-compiler來幫我壓縮js檔

--- MinifyJSTool.bat
+++ MinifyJSTool.bat
@@ -10,30 +10,58 @@ echo %cd%
 REM 加上/d才能跨槽轉換，先到原本的位子找出closure-compiler
 cd /d %~dp0
 echo %cd%
-for %%m in (closure-compiler*.jar) do ( REM 若發現多於的closure-compiler的話先刪掉舊版
-    echo find %%m
-)
+set minifier=closure-compiler-v20180402.jar
+REM for %%m in (closure-compiler*.jar) do ( REM 若發現多於的closure-compiler的話僅挑選最新版來使用
+REM     echo find %%m
+REM     if %%m GTR %minifier%  (
+REM         set minifier=%%m
+REM         echo come in, now minifier is %minifier%
+REM     ) 
+REM )
+echo %minifier%
+java -jar minifier CookieManager.js
 
+REM call :ScanFiles %1
+
+REM cd /d %1
+REM echo %cd%
+REM for /d %%d in (*) do ( REM 用來找出路徑下所有js
+REM     echo %%d
+REM     cd %%d
+REM     for %%i in (*.js) do (
+REM         echo %%d - %%i
+REM     )
+REM     cd ..
+REM )
+
+REM for %%m in (closure-compiler*.jar) do (
+REM     echo %%m is going to minify these js files
+REM     for %%i in (*.js) do (
+REM         echo %%i
+REM         REM java -jar %%m %%i> temp
+REM         REM type temp > %%i
+REM     )
+REM )
+REM del temp
+
+pause
+exit
+REM 程式結尾在此，下面為函式宣告，需要這麼寫的關係是因為批次檔做完上面的東西後還會自動去跑一次下面含是內的指令
+
+:ScanFiles
 cd /d %1
-echo %cd%
-for /d %%d in (*) do ( REM 用來找出路徑下所有js
-    echo %%d
-    cd %%d
-    for %%i in (*.js) do (
-        echo %%d - %%i
-    )
+for /d %%f in (*) do (
+    echo now at %%f
+    call :ScanFiles %%f
     cd ..
 )
-
-for %%m in (closure-compiler*.jar) do (
-    echo %%m is going to minify these js files
-    for %%i in (*.js) do (
-        echo %%i
-        REM java -jar %%m %%i> temp
-        REM type temp > %%i
-    )
+echo end of scanning
+for %%i in (*.js) do (
+    echo %%i
+    java -jar minifier %%i
+    REM java -jar minifier %%i> temp
+    REM type temp > %%i
 )
-REM del temp
 
 REM type motherfucker.txt
 REM type motherfucker.txt > Frank.txt
@@ -47,4 +75,3 @@ REM java -jar closure-compiler-v20180402.jar CookieManager.js > fuck.js
 REM set var=Peter
 REM set var=John
 REM echo %var%
-pause
---
+++ try.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set "foo=closure-compiler-v20180402.jar"
+
+echo %foo%
+
+set num=10
+
+echo %num%
+
+set /a num=num+1
+
+echo %num%
+
+pause
